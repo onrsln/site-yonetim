5f518ea20600ea09d3945da58d9fc8dfaa801630
onrsln <onur1667@gmail.com>
2026-01-15 21:20:02 +0300

    Fix: auth.ts type casting duzeltildi

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -51,15 +51,15 @@ export const authOptions: NextAuthOptions = {
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
-        token.id = user.id as string
-        token.role = (user as any).role as string
+        token.id = user.id
+        token.role = (user as any).role
       }
       return token
     },
     async session({ session, token }) {
       if (session.user) {
-        (session.user as any).id = token.id as string
-        (session.user as any).role = token.role as string
+        (session.user as any).id = token.id
+        (session.user as any).role = token.role
       }
       return session
     }
